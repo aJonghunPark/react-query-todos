2b23f6e1d40cd7b80d2e305b3d0a5f63f056bcb1
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-03-10 14:32:12 +0900

    Confirm the difference useContext and Redux

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ClassicalFetchA } from "./components/ClassicalFetchA";
 import { ClassicalFetchB } from "./components/ClassicalFetchB";
 import { Layout } from "./components/Layout";
+import { MainContext } from "./components/MainContext";
+import { MainRTKit } from "./components/MainRTKit";
 import { ReactQueryA } from "./components/ReactQueryA";
 import { ReactQueryB } from "./components/ReactQueryB";
 import { StateProvider } from "./context/StateProvider";
@@ -30,6 +32,8 @@ const App: FC = () => {
               <Route path="/query-b" element={<ReactQueryB />} />
               <Route path="/fetch-a" element={<ClassicalFetchA />} />
               <Route path="/fetch-b" element={<ClassicalFetchB />} />
+              <Route path="/main-context" element={<MainContext />} />
+              <Route path="/main-rtkit" element={<MainRTKit />} />
             </Routes>
           </Layout>
         </StateProvider>
